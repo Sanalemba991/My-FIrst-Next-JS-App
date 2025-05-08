22e8ac2b120900c37a03d78c6b4704135f11aa3a
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-05-08 16:47:32 +0530

    metadata change default by slug ProductId

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default async function notFound() {
+  return (
+    <div>
+      <h2>Pafge not FOund</h2>
+      <p>Sorry not found</p>
+    </div>
+  )
+}
+
+
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: { productId: string };
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = params.productId;
+
+  return {
+    title: `Product ${id}`,
+  };
+};
+
 export default async function  productDetails({
     params,
 }: {
